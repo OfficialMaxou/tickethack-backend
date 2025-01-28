fdc993d50da455dec20c006d9dd1fe51bf042403
Maxime Benard <maxime.benard.maxime@gmail.com>
2025-01-28 11:34:46 +0100

    set up mongoose moment connectionjs  tickethack backend

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// lien connexion
+require('../models/connection')
+const Trip = require('../models/trips')
+const Booking =require('../models/bookings')
+const Cart = require('../models/carts')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
