cb6aa82823ce7fb8afa2bc0571565cfc0c188614
Maxime Benard <maxime.benard.maxime@gmail.com>
2025-01-28 19:47:08 +0100

    CA MARCHEgit add .git add .

--- routes/trips.js
+++ routes/trips.js
@@ -8,27 +8,27 @@ const Trip = require('../models/trips')
 const { checkBody } = require('../modules/checkBody')
 const moment = require('moment')
 
-
+/*
 router.get('/', function(req, res, next) {
     Trip.find().then(data=>{
         res.json({ trips: data })
     })
-  });
+  });*/
               
  router.post('/', (req, res) => {
   if (!checkBody(req.body, ['departure', 'arrival', 'date'])) {
       res.json({ result: false, error: 'Missing or empty fields'})
       return;
-  }     
-     Trip.find({ departure : { $regex: new RegExp(req.body.departure, 'i')},
-     arrival : { $regex: new RegExp(req.body.arrival, 'i')} ,
+  }    
+     Trip.find({ departure : { $regex: new RegExp(`^${req.body.departure}$`, 'i')},
+     arrival : { $regex: new RegExp(`^${req.body.arrival}$`, 'i')} ,
       date : {
               $gte: moment.utc(req.body.date).startOf("day").toDate(),
               $lt: moment.utc(req.body.date).endOf("day").toDate(),},
       })
       .then(VilleMatch => {
       if (VilleMatch) {
-        res.json({ VilleMatch});
+        res.json({ result : true, VilleMatch});
       } else {
         res.json({ result: false, error: 'No trip found' });
           }
